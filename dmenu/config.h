97dfe7c71afc7506246e8c016a11977de64654ba
B0hrer <martin.bielefeld@hotmail.de>
2022-04-17 10:24:21 +0200

    updated dmenu theme

--- dmenu/config.h
+++ dmenu/config.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"FiraCode-Regular:size=10", "NotoColorEmoji:size=10"
+	"Jetbrains Mono-Medium:size=12", "NotoColorEmoji:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#f783bf" },
-	[SchemeSel] = { "#eeeeee", "#f783bf" },
-	[SchemeOut] = { "#000000", "#f783bf" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#bb83f7" },
+	[SchemeOut] = { "#000000", "#bb83f7" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
